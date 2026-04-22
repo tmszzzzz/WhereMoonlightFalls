7768e37cc2b8352227cb174fa557c58784c330f0
云起时 <1227233368@qq.com>
2026-04-22 15:10:17 +0800

    2026.4.22

--- Luas/true_end_2.lua
+++ Luas/true_end_2.lua
@@ -2,10 +2,17 @@ local calc = require("#Monocle.Calc")
 local color = require("#Microsoft.Xna.Framework.Color")
 
 function onBegin()
+    makeUnskippable()
+    disableRetry()
     disableMovement()
     player.ForceCameraUpdate = true
+    player.Speed = vector2(0,0)
+    player.Facing = getEnum("Celeste.Facings", "Right")
+    
+    setFlag("true_ending", true)
     wait(2.5)
     say("tmszzzzz_WhereMoonlightFalls_LastScene5Dialog_true_end")
+    
     local level = getLevel()
     local doneFlag = "my_book_closed"
     level.Session:SetFlag(doneFlag, false)
@@ -24,15 +31,23 @@ function onBegin()
     end
 
     say("tmszzzzz_WhereMoonlightFalls_LastScene6Dialog_true_end")
+    
+    wait(1)
+    setFlag("showblackfg", true)
+    wait(3)
     player.ForceCameraUpdate = false
     enableMovement()
+    enableRetry()
+    endCutscene()
+    instantTeleportTo(16208, -17048, "R4-TE2")
 end
 
 function onEnd(room, wasSkipped)
+    enableRetry()
+    player.ForceCameraUpdate = false
+    enableMovement()
     -- 如果过场被跳过，也要保证最终状态成立
     if wasSkipped then
+        instantTeleportTo(16208, -17048, "R4-TE2")
     end
-
-    --player.ForceCameraUpdate = false
-    enableMovement()
 end
--- Luas/true_end_3.lua
+++ Luas/true_end_3.lua
@@ -4,22 +4,22 @@ local color = require("#Microsoft.Xna.Framework.Color")
 function onBegin()
     disableMovement()
     player.ForceCameraUpdate = true
-    getLevel():Flash(color.White)
+    wait(3)
+    setFlag("showblackfg", false)
     player.Speed = vector2(0,0)
     player.Facing = getEnum("Celeste.Facings", "Right")
-    instantTeleportTo(-2532, 598, "R4-TE1")
-    playSound("event:/game/04_cliffside/gondola_scaryhair_01")
     player.ForceCameraUpdate = false
     enableMovement()
     endCutscene()
 end
 
 function onEnd(room, wasSkipped)
-    -- 如果过场被跳过，也要保证最终状态成立
-    if wasSkipped then
-    end
-    setFlag("true_ending", false)
-
+    
     player.ForceCameraUpdate = false
     enableMovement()
+    -- 如果过场被跳过，也要保证最终状态成立
+    if wasSkipped then
+        setFlag("showblackfg", false)
+    end
+
 end
